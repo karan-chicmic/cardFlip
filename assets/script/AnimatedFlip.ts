6bcabd01f03dc95c3d8a925a9bc8d4a765ac4f03
Abhishek Rawat <abhishek.rawat@chicmic.co.in>
2024-04-17 16:47:17 +0530

    working, infinite pending and random number

--- assets/script/AnimatedFlip.ts
+++ assets/script/AnimatedFlip.ts
@@ -1,23 +1,9 @@
-import {
-    _decorator,
-    Component,
-    EventMouse,
-    instantiate,
-    Label,
-    Node,
-    Prefab,
-    randomRangeInt,
-    SpriteFrame,
-    tween,
-    UITransform,
-    Vec3,
-} from "cc";
+import { _decorator, Component, EventMouse, instantiate, Label, Node, Prefab, randomRangeInt, tween, Vec3 } from "cc";
 import { addImage } from "./addImage";
 const { ccclass, property } = _decorator;
 
-const SPRITE_COUNT = 6;
-@ccclass("flip")
-export class flip extends Component {
+@ccclass("AnimatedFlip")
+export class AnimatedFlip extends Component {
     @property({ type: Prefab })
     cardPrefab: Prefab;
 
@@ -31,7 +17,7 @@ export class flip extends Component {
         for (let i = 0; i < this.totalCard; i++) {
             const card = instantiate(this.cardPrefab);
             // card.getComponent(addImage).setCard();
-            card.getComponent(addImage).setBackground();
+            // card.getComponent(addImage).setBackground();
 
             if (i % 2 == 0) {
                 card.setPosition(-750 + i * 200, 200, 0);
@@ -44,11 +30,14 @@ export class flip extends Component {
             if (i == this.totalCard - 1 && !(this.totalCard % 2 == 0)) {
                 card.setPosition(this.lastSecondCard.getPosition().x + 350, 0, 0);
             }
-            this.node.addChild(card);
+
+            setTimeout(() => {
+                this.node.addChild(card);
+            }, 1000 * i);
 
             card.on(
                 Node.EventType.MOUSE_DOWN,
-                (event: EventMouse) => {
+                (event) => {
                     this.onSpriteClick(event);
                 },
 
@@ -62,21 +51,21 @@ export class flip extends Component {
     onSpriteClick(event: EventMouse) {
         var label = instantiate(this.labelPrefab);
         const clickedSprite = event.currentTarget;
-        this.node.addChild(label);
+        this.node.getParent().addChild(label);
 
         console.log(this.node.getParent().children);
         tween(clickedSprite)
-            .to(0.3, { scale: new Vec3(0, 1, 0) })
-            // .to(0.6, {scale: new Vec3(0, 0, 0)})
+            //     .to(0.3, { scale: new Vec3(0, 1, 0) })
+            //     // .to(0.6, {scale: new Vec3(0, 0, 0)})
             .call(() => {
-                clickedSprite.getComponent(addImage).setCard();
+                // clickedSprite.getComponent(addImage).setCard();
                 // clickedSprite.addChild(label);
                 label.addComponent(Label);
                 label.setPosition(clickedSprite.getPosition());
                 label.getComponent(Label).string = randomRangeInt(0, 10).toString();
                 console.log(label.getComponent(Label).string);
             })
-            .to(0.3, { scale: new Vec3(1, 1, 0) })
+            //     .to(0.3, { scale: new Vec3(1, 1, 0) })
             .start();
 
         // console.log(label.getComponent(Label).string);
